38c79b940f625674ca0e9c7493684b4fa7aa00a6
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2020-04-25 01:39:35 +0300

    hot fix

--- lib/main.dart
+++ lib/main.dart
@@ -22,24 +22,23 @@ class MyApp extends StatelessWidget {
             textTheme: ButtonTextTheme.accent,
           ),
         ),
-      ),
-      home: Gate(),
-      supportedLocales: [const Locale('tr'), const Locale('en')],
-      localizationsDelegates: [
+        home: Gate(),
+        supportedLocales: [const Locale('tr'), const Locale('en')],
+        localizationsDelegates: [
           AppLocalizationDelegate(),
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
           GlobalCupertinoLocalizations.delegate
-      ],
-      localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
-        if (locale == null) {
-          debugPrint("*language locale is null!!!");
-          return supportedLocales.first;
-        }
-        for (Locale supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale.languageCode ||
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
+        ],
+        localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
+          if (locale == null) {
+            debugPrint("*language locale is null!!!");
+            return supportedLocales.first;
+          }
+          for (Locale supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode || supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
           }
 
           return supportedLocales.first;
